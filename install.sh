9e50d51bfb2b12d36d0e59683aae4f8088118452
liviucoman10 <liviu.coman10@gmail.com>
2022-05-26 17:41:36 +0100

    update with an install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# define variables used by the script
+PATH_PREFIX=$HOME/.local/bin/scripthub
+MAIN_SCRIPT=scripthub
+MAIN_SCRIPT_URL=https://github.com/devgabrielcoman/scriptexchange-aliasengine/raw/main/build/scripthub
+
+# delete everything in install path
+rm -rf $PATH_PREFIX/
+echo "Cleaned out source folder" $PATH_PREFIX
+mkdir $PATH_PREFIX
+echo "Recreated source folder" $PATH_PREFIX
+
+# copy remote script to local
+main_script_path=$PATH_PREFIX/$MAIN_SCRIPT
+
+# download
+curl $MAIN_SCRIPT_URL --output $main_script_path
+echo "Downloaded ScriptHub app"
+
+# setup default command
+echo "alias scripthub=$main_script_path" >> $PATH_PREFIX/.aliases
+echo "Setup ScriptHub"
