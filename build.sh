1b2c1feee029bd2c5591b5c9606bf0c37b351c9b
liviucoman10 <liviu.coman10@gmail.com>
2022-06-19 18:30:12 +0100

    update

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# inspired by 
+# https://www.digitalocean.com/community/tutorials/how-to-build-go-executables-for-multiple-platforms-on-ubuntu-16-04
+
+package_name=bbee
+
+cd aliasengine
+
+platforms=("darwin/arm64" "darwin/amd64" "linux/386" "linux/arm64" "linux/amd64")
+
+for platform in "${platforms[@]}"
+do
+	platform_split=(${platform//\// })
+	GOOS=${platform_split[0]}
+	GOARCH=${platform_split[1]}
+	output_name=$package_name'-'$GOOS'-'$GOARCH
+	if [ $GOOS = "windows" ]; then
+		output_name+='.exe'
+	fi	
+
+	env GOOS=$GOOS GOARCH=$GOARCH go build -o ../build/$output_name $package
+	if [ $? -ne 0 ]; then
+   		echo 'An error has occurred! Aborting the script execution...'
+		exit 1
+	fi
+done
