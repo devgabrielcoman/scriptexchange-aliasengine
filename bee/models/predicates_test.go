ab38d2b2bb46de1c9019e21c7e6d1c0bbbd06229
liviucoman10 <liviu.coman10@gmail.com>
2022-07-03 16:23:24 +0100

    tests for unique items

--- bee/models/predicates_test.go
+++ bee/models/predicates_test.go
@@ -71,3 +71,64 @@ func Test_FilterByPath(t *testing.T) {
 		assert.Equal(t, expected, result)
 	})
 }
+
+func Test_UniqueItems(t *testing.T) {
+	t.Run("should return an empty slice given an empty input", func(t *testing.T) {
+		input := []IndexItem{}
+		result := UniqueItems(input)
+		expected := []IndexItem{}
+		assert.Equal(t, expected, result)
+	})
+
+	t.Run("should return same slice in case there are no duplicates", func(t *testing.T) {
+		item1 := IndexItem{
+			Name:       "One",
+			Path:       "/my_path",
+			Content:    "Content",
+			Comments:   []string{},
+			PathOnDisk: "/full/my_path",
+			Type:       ScriptType(Alias),
+			Date:       123,
+		}
+		item2 := IndexItem{
+			Name:       "Two",
+			Path:       "/my_other_path",
+			Content:    "Content",
+			Comments:   []string{},
+			PathOnDisk: "/full/my_other_path",
+			Type:       ScriptType(Alias),
+			Date:       123,
+		}
+
+		items := []IndexItem{item1, item2}
+		expected := []IndexItem{item1, item2}
+		result := UniqueItems(items)
+		assert.Equal(t, expected, result)
+	})
+
+	t.Run("should return a slice of unique items given a slice with duplicates", func(t *testing.T) {
+		item1 := IndexItem{
+			Name:       "One",
+			Path:       "/my_path",
+			Content:    "Content",
+			Comments:   []string{},
+			PathOnDisk: "/full/my_path",
+			Type:       ScriptType(Alias),
+			Date:       123,
+		}
+		item2 := IndexItem{
+			Name:       "Two",
+			Path:       "/my_other_path",
+			Content:    "Content",
+			Comments:   []string{},
+			PathOnDisk: "/full/my_other_path",
+			Type:       ScriptType(Alias),
+			Date:       123,
+		}
+
+		items := []IndexItem{item1, item1, item2}
+		expected := []IndexItem{item1, item2}
+		result := UniqueItems(items)
+		assert.Equal(t, expected, result)
+	})
+}
